3c79601de4d4acba6c2b77af6dff86db7937b102
Ek Bunthen <e.bunthen@cmedcc.com>
2025-08-12 10:25:27 +0000

    Update file Program.cs

--- cmedcc-idass.backend/Program.cs
+++ cmedcc-idass.backend/Program.cs
@@ -70,7 +70,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
     app.UseSwagger();
     app.UseDeveloperExceptionPage();
-    app.UsePathBase("/cmedcc-idass-backend");
+    app.UsePathBase("/cmedcc-idaas-backend");
 }
 //Middleware registering
 //app.UseMiddleware<ApiKeyMiddleware>();
