a34cdacefb5ad8a275148b158bf59be7833cabdb
bunthen3 <bebunthen@gmail.com>
2025-08-12 08:10:45 +0700

    Update ignore file

--- cmedcc-idass.backend/obj/Debug/net9.0/cmedcc-idass.backend.AssemblyInfo.cs
+++ cmedcc-idass.backend/obj/Debug/net9.0/cmedcc-idass.backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cmedcc-idass.backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04a9744bf28afc7108d57d89fe5ca1b14931c28d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86afad2688258b07c44de0d202f57ce7ddecd7b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("cmedcc-idass.backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cmedcc-idass.backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
