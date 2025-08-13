0f9768233a963c56fe7f651b0b8d3599f4f2e0f0
bunthen3 <bebunthen@gmail.com>
2025-08-13 13:11:25 +0700

    update deployment

--- cmedcc-idass.backend/obj/Debug/net9.0/cmedcc-idass.backend.AssemblyInfo.cs
+++ cmedcc-idass.backend/obj/Debug/net9.0/cmedcc-idass.backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cmedcc-idass.backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d67c85406e67d7dbbcf5773a1a5565c8ab5cd214")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c53cc5c069bf9bb6125c8eea8e72897f321197cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("cmedcc-idass.backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cmedcc-idass.backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
